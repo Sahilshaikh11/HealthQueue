34c64d08d97c87a618e16998f8216cb8ff54ed46
Shahil Shaikh <shaikhsahil4495@gmail.com>
2025-03-04 16:46:43 +0530

    backend dev started(configuration,addDoctor API, admin login API)

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+connectDB();
+connectCloudinary();
+
+app.use("/api/admin", adminRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
